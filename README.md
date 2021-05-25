fd4b68a245b5129064dfec65aa404759c127e74e
dcurator330 <amicokb330@gmail.com>
2021-05-25 21:39:19 +0900

    add caex6

---
+++ README.md
@@ -0,0 +1,16 @@
+콘텐츠 편집 기능(Bookmark, Blog)
+Owner 필드 추가 - models.py
+변경사항 db 반영
+url 정의
+뷰 로직 및 OwnerOnlyMixin 작성
+템플릿 파일 및 403.html 작성
+콘텐츠 편집 기능(Photo)
+Owner 필드 추가
+변경사항 db 반영
+url 정의
+인라인 폼셋 정의
+뷰 로직 작성
+템플릿 파일 작성
+home.html 수정
+
+ 
---
+++ ch99/blog/views.py
@@ -0,0 +1,97 @@
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic import DayArchiveView, TodayArchiveView
+from django.views.generic import FormView
+from django.conf import settings
+from django.db.models import Q
+from django.shortcuts import render
+from blog.models import Post
+from blog.forms import PostSearchForm
+
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from mysite.views import OwnerOnlyMixin
+
+#--- ListView
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+#--- DetailView
+class PostDV(DetailView):
+    model = Post
+
+
+#--- ArchiveView
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    make_object_list = True
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+#--- FormView
+class SearchFormView(FormView): 
+    form_class = PostSearchForm 
+    template_name = 'blog/post_search.html' 
+
+    def form_valid(self, form): 
+        searchWord = form.cleaned_data['search_word']
+        post_list = Post.objects.filter(Q(title__icontains=searchWord) |  Q(description__icontains=searchWord) | Q(content__icontains=searchWord)).distinct()
+
+        context = {} 
+        context['form'] = form 
+        context['search_term'] = searchWord 
+        context['object_list'] = post_list 
+
+        return render(self.request, self.template_name, context)   # No Redirection
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content']
+    initial = {'slug': 'auto-filling-do-not-input'}    
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class PostChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'blog/post_change_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(owner=self.request.user)
+
+
+class PostUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content']
+    success_url = reverse_lazy('blog:index')
+
+
+class PostDeleteView(OwnerOnlyMixin, DeleteView) :
+    model = Post
+    success_url = reverse_lazy('blog:index')
